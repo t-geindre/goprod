120244f474fb31c823bb942d3e533476ab62a3bd
Thierry Geindre <thierry@zol.fr>
2017-01-24 00:44:49 +0100

    Add pr search

--- src/AppBundle/Resources/public/js/component/github/github-issues.js
+++ src/AppBundle/Resources/public/js/component/github/github-issues.js
@@ -1,9 +1,40 @@
 Vue.component('github-issues', {
+    delimiters: ['[[', ']]'],
     template: '#github-issues-template',
-    props: ['query', 'forceQuery', 'userId'],
+    props: ['query', 'queryAppend', 'userLogin'],
+    data: function() {
+        return {
+            pulls: [],
+            sort: 'created',
+            order: 'desc'
+        }
+    },
+    mounted: function() {
+        this.update();
+    },
     methods: {
         update: function() {
-
+            var data = {
+                q: this.query,
+                sort: this.sort,
+                order: this.order
+            };
+            if (this.queryAppend) {
+                data.q += ' '+this.queryAppend;
+            }
+            this.$github.searchIssues(data).then(function(response) {
+                this.pulls = response.data.items;
+            });
+        },
+        setSort: function(type, order) {
+            this.sort = type;
+            this.order = order;
+            this.update();
+        }
+    },
+    filters: {
+        "repo-name": function (value) {
+            return value.split('repos')[1].slice(1);
         }
     }
 });
--- src/AppBundle/Resources/public/js/component/user-details.js
+++ src/AppBundle/Resources/public/js/component/user-details.js
@@ -2,9 +2,6 @@ Vue.component('user-details', {
     template: '#user-details-template',
     props: ['authenticated', 'user'],
     delimiters: ['[[', ']]'],
-    mounted: function() {
-        console.log(this.user);
-    },
     methods: {
         login: function() {
             this.$emit('login');
