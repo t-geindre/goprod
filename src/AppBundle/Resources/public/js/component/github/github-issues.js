1aeb4629f5b16f01f058a3b1a29d86c301ef03bc
Thierry Geindre <thierry@zol.fr>
2017-01-23 23:50:01 +0100

    Add basic pr layout

---
+++ src/AppBundle/Resources/public/js/component/github/github-issues.js
@@ -0,0 +1,9 @@
+Vue.component('github-issues', {
+    template: '#github-issues-template',
+    props: ['query', 'forceQuery', 'userId'],
+    methods: {
+        update: function() {
+
+        }
+    }
+});
