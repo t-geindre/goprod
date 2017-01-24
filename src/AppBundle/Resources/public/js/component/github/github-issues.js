f4cf28b11500ea5867138e838a9dba7571710883
Thierry Geindre <github@nostalgeek.org>
2017-01-24 17:50:08 +0100

    Improve PR search

--- src/AppBundle/Resources/public/js/component/github/github-issues.js
+++ src/AppBundle/Resources/public/js/component/github/github-issues.js
@@ -4,9 +4,11 @@ Vue.component('github-issues', {
     props: ['query', 'queryAppend', 'userLogin'],
     data: function() {
         return {
-            pulls: [],
+            issues: [],
             sort: 'created',
-            order: 'desc'
+            order: 'desc',
+            open: true,
+            iam: 'author'
         }
     },
     mounted: function() {
@@ -20,21 +22,51 @@ Vue.component('github-issues', {
                 order: this.order
             };
             if (this.queryAppend) {
-                data.q += ' '+this.queryAppend;
+                data.q += ' ' + this.queryAppend;
             }
             this.$github.searchIssues(data).then(function(response) {
-                this.pulls = response.data.items;
+                this.issues = response.data.items;
             });
         },
         setSort: function(type, order) {
             this.sort = type;
             this.order = order;
             this.update();
+        },
+        queryUpdate: function(remove, add) {
+            var items = this.query.toLowerCase().split(' ');
+            remove.forEach(function(remove) {
+                while ((index = items.indexOf(remove)) > -1) {
+                    items.splice(index, 1);
+                }
+            })
+            items.unshift(add);
+            this.query = items.join(' ');
         }
     },
     filters: {
         "repo-name": function (value) {
             return value.split('repos')[1].slice(1);
+        }
+    },
+    watch: {
+        open: function() {
+            this.queryUpdate(
+                ['is:closed', 'is:open'],
+                this.open ? 'is:open' : 'is:closed'
+            );
+            this.update();
+        },
+        iam: function() {
+            this.queryUpdate(
+                [
+                    'author:'+this.userLogin,
+                    'assignee:'+this.userLogin,
+                    'mentions:'+this.userLogin
+                ],
+                this.iam+':'+this.userLogin
+            );
+            this.update();
         }
     }
 });
