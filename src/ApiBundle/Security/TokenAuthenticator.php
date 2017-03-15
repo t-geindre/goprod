70279e7b6e0ed48789e4bd19b7c72675e17d55d5
Thierry Geindre <github@nostalgeek.org>
2017-03-15 18:13:06 +0100

    Improving authentication system

---
+++ src/ApiBundle/Security/TokenAuthenticator.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace ApiBundle\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+/**
+ * API Token auth
+ */
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getCredentials(Request $request)
+    {
+        if (!$login = $request->headers->get('X-AUTH-LOGIN')) {
+            return;
+        }
+
+        if (!$token = $request->headers->get('X-AUTH-TOKEN')) {
+            return;
+        }
+
+        return [
+            'token' => $token,
+            'login' => $login,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $userProvider->getRepository()->findOneBy($credentials);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new JsonResponse(
+            ['message' => strtr($exception->getMessageKey(), $exception->getMessageData())],
+            Response::HTTP_FORBIDDEN
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new JsonResponse(
+            ['message' => 'Authentication Required'],
+            Response::HTTP_UNAUTHORIZED
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
